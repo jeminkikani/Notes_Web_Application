3756d12436b1cb2262cfb9ab8e6f992eee4264a9
skilljeminkikani <skilljeminkikani9510@gmail.com>
2023-08-13 11:20:35 +0530

    Notes_Web_Application

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const passport = require('passport');
 const MongoStore = require('connect-mongo');
 
 const app = express();
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT;
 
 app.use(session({
   secret: 'keyboard cat',
