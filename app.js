9e618a232accbfb3fa804f9a8049b10329d6cf6f
skilljeminkikani <skilljeminkikani9510@gmail.com>
2024-02-14 18:06:46 +0530

    changes

--- app.js
+++ app.js
@@ -1,57 +1,56 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const methodOverride = require("method-override");
-const connectDB = require('./server/config/db');
-const session = require('express-session');
-const passport = require('passport');
-const MongoStore = require('connect-mongo');
+const connectDB = require("./server/config/db");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI
-  }),
-  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) }   
-  // Date.now() - 30 * 24 * 60 * 60 * 1000
-}));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) }
+    // Date.now() - 30 * 24 * 60 * 60 * 1000
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
 // Conntect to Database
-connectDB();  
+connectDB();
 
 // Static Files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Templating Engine
 app.use(expressLayouts);
-app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
-
-
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
 
 // Routes
-app.use('/', require('./server/routes/auth'));
-app.use('/', require('./server/routes/index'));
-app.use('/', require('./server/routes/dashboard'));
+app.use("/", require("./server/routes/auth"));
+app.use("/", require("./server/routes/index"));
+app.use("/", require("./server/routes/dashboard"));
 
 // Handle 404
-app.get('*', function(req, res) {
+app.get("*", function (req, res) {
   //res.status(404).send('404 Page Not Found.')
-  res.status(404).render('404');
-})
-
+  res.status(404).render("404");
+});
 
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
